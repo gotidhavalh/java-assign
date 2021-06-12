a372b9ffba587aa71789fb8accbf11e832435ba4
Dhaval Goti <gotidhavalh@gmail.com>
2021-06-12 11:52:09 +0530

    Update JavaAssignApplication.java
    update

--- src/main/java/com/assignment/javaassign/JavaAssignApplication.java
+++ src/main/java/com/assignment/javaassign/JavaAssignApplication.java
@@ -13,7 +13,6 @@ public class JavaAssignApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaAssignApplication.class, args);
 		
-		System.out.println( " Test - one ");
 	}
 
 }
